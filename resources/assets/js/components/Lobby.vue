3d1ae0b546ac0314b6e3691b92dc4835d6253d5a
Joosep Alviste <joosep.alviste@gmail.com>
2017-04-28 00:07:24 +0300

    Refactor into Lobby component

---
+++ resources/assets/js/components/Lobby.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Lobby
+                    </div>
+
+                    <div class="card-block lobby-block">
+                        <user-list :users_in_lobby="usersInLobby"></user-list>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                usersInLobby: [
+                    {
+                        id: 1,
+                        name: 'Brenda'
+                    },
+                    {
+                        id: 2,
+                        name: 'Joosep'
+                    },
+                ]
+            }
+        }
+    }
+</script>
--- resources/views/lobby.blade.php
+++ resources/views/lobby.blade.php
@@ -2,20 +2,6 @@
 
 @section('content')
 
-    @component('layouts.components.center-card')
-
-        @slot('header')
-            Lobby
-        @endslot
-
-        @slot('cardBlockClass')
-            lobby-block
-        @endslot
-
-        @slot('content')
-            <user-list :users_in_lobby="usersInLobby"></user-list>
-        @endslot
-
-    @endcomponent
+    <lobby></lobby>
 
 @endsection
