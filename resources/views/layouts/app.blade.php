3d5dd1646506ecabd8b627639064f7deb590c7b7
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-06 01:29:42 +0300

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
     <title>{{ config('app.name', 'Typonaut') }}</title>
 
     <!-- Styles -->
